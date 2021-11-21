1e9898fe3953f5e0db2e3e863f9e04ff87a9d287
Consuela Vlek <consuelavlek@Consuelas-MacBook-Air.local>
2021-11-21 17:16:46 +0100

    added routes

--- ironCohort/routes/auth.js
+++ ironCohort/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -15,16 +15,17 @@ router.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
     });
-
+   
     //save user and respond
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
+    console.log(err)
     res.status(500).json(err)
   }
 });
 
-//LOGIN
+// LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
--- ironCohort/routes/users.js
+++ ironCohort/routes/users.js
@@ -1,11 +1,12 @@
 const User = require("../models/User");
 const router = require("express").Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcryptjs");
 
 //update user
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
+      
       try {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
@@ -17,12 +18,12 @@ router.put("/:id", async (req, res) => {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
-      res.status(200).json("Account has been updated");
+      res.status(200).json("Your account has been updated");
     } catch (err) {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can update only your account!");
+    return res.status(403).json("You can only update your account!");
   }
 });
 
@@ -36,7 +37,7 @@ router.delete("/:id", async (req, res) => {
       return res.status(500).json(err);
     }
   } else {
-    return res.status(403).json("You can delete only your account!");
+    return res.status(403).json("You can only delete your account!");
   }
 });
 
