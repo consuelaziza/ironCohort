f3a2a172b7204ca73783029282df1b85f521d20a
Consuela Vlek <consuelavlek@Consuelas-MacBook-Air.local>
2021-11-24 17:07:22 +0100

    new repo who dis

--- ironCohort/routes/gather.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const Post = require("../models/Post")
-
-router.get('/gather', (req, res, ) => {
-    res.render('auth/gather.hbs')
-  })
-
-  module.exports = router;
--- ironCohort/routes/home.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const Post = require("../models/Post")
-
-router.get('/home', (req, res, ) => {
-    res.render('auth/home.hbs')
-  })
-
-  module.exports = router;
